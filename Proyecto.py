4e3763b6aafc349417485fcc7f0206f2f5f7e686
UlisesArroyo <66101708+UlisesArroyo@users.noreply.github.com>
2020-12-23 23:34:29 -0600

    Se muestran 3 imagenes BASE

--- Proyecto.py
+++ Proyecto.py
@@ -10,20 +10,22 @@ raiz = Tk()  #ventana
 
 raiz.title("Ventana de pruebas")
 raiz.config(bg = "orange")
-
+ruta_1= "img/mi_amor_1.jpg"
+ruta_2= "img/mi_amor_2.jpg"
+ruta_3= "img/mi_amor_3.jpg"
 miframe = Frame(raiz)
 miframe.pack(fill="both",expand ="True")
 miframe.config(bg = "black")
 miframe.config(width = "1280",height = "720")
 
-#Ejemplo
+"""
 img3 = Image.open("C:/Users/HP/Desktop/Pruebas/img/1CV12_12_R_#89_003809.jpg") 
 img3 = img3.resize((320, 240), Image.ANTIALIAS)
 img3 = ImageTk.PhotoImage(image=img3)
 Imagen3 = Label(miframe, image=img3)
 Imagen3.pack(side="bottom", fill="both", expand="yes")
 Imagen3.place(x=740,y=200)
-
+"""
 
 
 def abrir_carpeta():
@@ -58,35 +60,35 @@ def abrir_carpeta():
 
 
 
-def openimg(ruta):
 	
-	imagen = Image.open(ruta[3])
-	imagen = imagen.resize((320, 240), Image.ANTIALIAS)   #con esta madre hacemos mas chicas las imagenes de los costados para que se vea mamalon
-	imagen = ImageTk.PhotoImage(image=imagen)          
-	Imagen1 = Label(miframe, image=imagen)
-	Imagen1.pack(side="bottom", fill="both", expand="yes")
-	Imagen1.place(x=100,y=200)
+imagen = Image.open(ruta_1)
+imagen = imagen.resize((320, 240), Image.ANTIALIAS)   #con esta madre hacemos mas chicas las imagenes de los costados para que se vea mamalon
+imagen = ImageTk.PhotoImage(image=imagen) 
+img2 = Image.open(ruta_2) 
+img2 = img2.resize((320, 240), Image.ANTIALIAS)
+img2 = ImageTk.PhotoImage(image=img2) 
+img3 = Image.open(ruta_3) 
+img3 = img3.resize((320, 240), Image.ANTIALIAS)
+img3 = ImageTk.PhotoImage(image=img3)
+	
+
+Imagen1 = Label(miframe, image=imagen)
+Imagen1.pack(side="bottom", fill="both", expand="yes")
+Imagen1.place(x=100,y=200)
     
-	'''
-	img2 = Image.open(ruta[1]) 
-	img2 = img2.resize((320, 240), Image.ANTIALIAS)
-	img2 = ImageTk.PhotoImage(image=img2)         #Esta madre debe aparecer mas grande ya que sera la central
-	Imagen2 = Label(miframe, image=img2)
-	Imagen2.pack(side="bottom", fill="both", expand="yes")
-	Imagen2.place(x=420,y=200)
 
-	img3 = Image.open(ruta[2]) 
-	img3 = img3.resize((320, 240), Image.ANTIALIAS)
-	img3 = ImageTk.PhotoImage(image=img3)
-	Imagen3 = Label(miframe, image=img3)
-	Imagen3.pack(side="bottom", fill="both", expand="yes")
-	Imagen3.place(x=740,y=200)
-	'''
+#Esta madre debe aparecer mas grande ya que sera la central
+Imagen2 = Label(miframe, image=img2)
+Imagen2.pack(side="bottom", fill="both", expand="yes")
+Imagen2.place(x=420,y=200)
+
+Imagen3 = Label(miframe, image=img3)
+Imagen3.pack(side="bottom", fill="both", expand="yes")
+Imagen3.place(x=740,y=200)
 
 
 
-	print("es la ruta 1",ruta[0])
-	print("es la ruta 2",ruta[1])
+
 
     
 
