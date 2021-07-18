0a506a6dd604384a583e57f7f08e30d8d2d13485
UlisesArroyo <arroyorojasulisesdan@gmail.com>
2021-07-18 01:24:36 -0500

    f

--- Proyecto.py
+++ Proyecto.py
@@ -96,7 +96,7 @@ lblFondo = Label(miframe,image=imgFondo).place(x=0,y=0)
 
 def abrir_carpeta():
 	global rutasImagenes,carpetaAbierta, posicionImagen, rutaRelativa
-	posicionImagen = 3472
+	posicionImagen = 3735
 	archivo_abierto = filedialog.askdirectory(initialdir="./", title="Select file") 
 
 	if(len(archivo_abierto) != 0):
